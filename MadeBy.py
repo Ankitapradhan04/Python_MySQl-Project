ef3e2b69315e93f592594a62698c8b49a3b563cc
Ankita Pradhan <67707036+Ankitapradhan04@users.noreply.github.com>
2021-10-12 10:03:02 +0530

    Create MadeBy.py

---
+++ MadeBy.py
@@ -0,0 +1,15 @@
+def made_by():
+    msg = ''' 
+            Parking Management system made by           : ANKITA PRADHAN
+            School Name                                 : KVS
+            session                                     : 2020-21
+            
+            Thanks for evaluating my Project.
+            \n\n\n
+        '''
+
+    for x in msg:
+        print(x, end='')
+        time.sleep(0.002)
+
+    wait = input('Press any key to continue.....')
